0c52bf001fe21fb28589b51db64a60cfdc2a6ae1
pppeum <s2018s13@e-mirim.hs.kr>
2019-10-17 23:16:45 +0900

    add sign-form and logo

---
+++ sign-up.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+
+    $username = "";
+    $email = "";
+    $nickname = "";
+    $errors = array();
+
+    $conn = mysqli_connect('127.0.0.1', 'root', 'mirim2', 'hauso');
+    $data_stream = "'".$_POST['username']."', '".$_POST['password']."', '".$_POST['name']."', '".$_POST['nick']."', '".$_POST['email']."'";
+    
+    $query = "insert into member(username, password, name, nick, email) values(".$data_stream.")";
+    $result = mysqli_query($conn, $query);
+
+    if($result)
+        echo "1";
+    else
+        echo "-1";
+
+    mysqli_close($conn);
+?>
